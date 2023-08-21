d24f07b63c965dad3b7b6dc477478c6dba26d094
Farmer-Eds-Shed <>
2023-08-21 16:10:34 +0000

    GET API key from ENV Variable

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import requests
-import api_key
+import os
+import json
 
 lat = 53.6471
-lon = 6.6967
-api_key = api_key.api_key
+lon = -6.6967
+api_key = os.getenv('OWM_API_KEY')
 response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}")
-print(response.json())
-print(api_key)
+
+data = json.loads(response.text)
+print(data["list"])
